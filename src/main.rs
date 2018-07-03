267476ac0fa35860dcccbee6d8202486bee84083
Johannes Hölzl <johannes.hoelzl@gmx.de>
2018-07-03 19:00:14 +0200

    finish parser

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,118 @@
 #[macro_use]
 extern crate nom;
 
+use std::{env, fs, str};
+
 use nom::multispace;
 
-/* horrible ASCII parser for UTF-8 input... */
 
-named!(multi_line_comment( &[u8] ) -> (), do_parse!(tag!("/-") >> take_until!("-/") >> ()));
+// This is an ASCII only parser for Lean's import statements. Lean's --deps argument parser doesn't
+// work in version 3.4.1.
 
-named!(line_comment( &[u8] ) -> (), do_parse!(tag!("--") >> take_until!("\r") >> ()));
+#[inline]
+fn is_name_char(c : char) -> bool {
+  c.is_alphanumeric() || c == '_'
+}
 
-named!(whitespace( &[u8] ) -> (),
-  do_parse!(many0!(alt!(do_parse!(multispace >> ()) | multi_line_comment | line_comment)) >> ()));
+named!(whitespace( &str ) -> (), do_parse!(
+  many0!(alt!(
+      do_parse!(multispace >> ())
+    | do_parse!(tag_s!("/-") >> take_until_and_consume!("-/") >> ())
+    | do_parse!(tag_s!("--") >> take_until_either_and_consume!("\r\n") >> ()))) >>
+  ()));
 
-named!(prelude( &[u8] ) -> (), do_parse!(tag!("prelude") >> ()));
+named!(word( &str ) -> Vec<&str>, do_parse!(
+  whitespace >>
+  n: separated_list!(tag_s!("."), take_while_s!(is_name_char)) >>
+  (n)));
 
-named!(import( &[u8] ) -> (), do_parse!(tag!("import") >> ()));
+const PRELUDE : &'static str = "prelude";
+
+const IMPORT : &'static str = "import";
 
 const KEYWORDS : &'static [&'static str] = &[
-  "conjecture",
-  "constant",
-  "constants",
-  "corollary",
-  "declare_trace",
-  "def",
-  "hypothesis",
-  "lemma",
-  "meta",
-  "noncomputable",
-  "parameter",
-  "parameters",
-  "protected",
-  "private",
-  "variable",
-  "variables",
-  "theorem",
-  "example",
-  "open",
-  "export",
+  "abbreviation",
+  "add_key_equivalence",
+  "attribute",
   "axiom",
   "axioms",
+  "class",
+  "coinductive",
+  "constant",
+  "constants",
+  "definition",
+  "declare_trace",
+  "example",
+  "export",
+  "hide",
+  "include",
   "inductive",
-  "structure",
-  "universe",
-  "universes",
-  "precedence",
-  "reserve",
   "infix",
   "infixl",
   "infixr",
-  "notation",
-  "postfix",
-  "prefix",
+  "init_quotient",
   "instance",
-  "namespace",
-  "section",
-  "attribute",
   "local",
-  "set_option",
-  "include",
-  "omit",
-  "class",
+  "meta",
   "mutual",
-  "run_command"
+  "namespace",
+  "noncomputable",
+  "notation",
+  "parameter",
+  "parameters",
+  "precedence",
+  "prefix",
+  "private",
+  "protected",
+  "postfix",
+  "reserve",
+  "run_cmd",
+  "omit",
+  "open",
+  "section",
+  "set_option",
+  "structure",
+  "theorem",
+  "universe",
+  "universes",
+  "variable",
+  "variables"
 ];
 
-
 fn main() {
-    println!("Hello, world!");
+  let mut args = env::args();
+  let _ = args.next().unwrap();
+  let module = args.next().expect("filename expected as first argument");
+
+  print!("file {:}\n", module);
+
+  let mut contents : &str = &fs::read_to_string(module).expect("module not found");
+  let mut prelude = false;
+  let mut modules = Vec::new();
+
+  loop {
+    match word(&contents) {
+      Ok((continuation, word)) => {
+        contents = continuation;
+        if word.len() == 1 {
+          let identifier : &str = word.first().unwrap();
+          if identifier == PRELUDE { prelude = true; }
+          else if identifier == IMPORT { }
+          else if KEYWORDS.contains(&identifier) { break; }
+          else { modules.push(word); }
+        } else {
+          modules.push(word);
+        }
+      },
+      Err(_e) => {
+        panic!("parser error");
+      }
+    }
+  }
+
+  print!("Dependencies (prelude: {:})\n", prelude);
+  for name in modules {
+    let n : String = name.join(".");
+    print!("  {:}\n", n);
+  }
 }
